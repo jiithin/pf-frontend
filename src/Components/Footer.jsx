a71fd8827c7b4a55cefbf4e0c93816c281d29aa7
Jithin <153214952+jiithin@users.noreply.github.com>
2024-09-01 22:35:02 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -40,7 +40,7 @@ function Footer() {
 <section className=''>
   <MDBContainer className='text-center text-md-start mt-5 p-4'>
     <MDBRow className='mt-3'>
-      <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+      <MDBCol md="4" lg="4" xl="4" className='mx-auto mb-4'>
         <h5 className='text-uppercase fw-bold mb-4'>
           <i class="fa-solid fa-diagram-project"></i> 
           Project Fair
@@ -50,7 +50,7 @@ function Footer() {
         </p>
       </MDBCol>
 
-      <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+      <MDBCol md="4" lg="2" xl="2" className='mx-auto mb-4'>
         <h5 className='text-uppercase fw-bold mb-4'>Links</h5>
         
         <p >
@@ -72,31 +72,20 @@ function Footer() {
       
       </MDBCol>
 
-      <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-        <h5 className='text-uppercase fw-bold mb-4'>Guides</h5>
-        
-      <p><Link to={'https://react.dev/'} style={{ textDecoration:'none'}}>React</Link></p>
-
-      <p><Link to={'https://react-bootstrap.netlify.app/'} style={{ textDecoration:'none'}}>React Bootsrtap</Link></p>
-       
-      <p><Link to={'https://bootswatch.com/'} style={{ textDecoration:'none'}}>BootsWatch</Link></p>
-       
-      
-      </MDBCol>
 
       
 
       <MDBCol md="4" lg="4" xl="5" className='mx-auto mb-md-0 mb-4'>
-        <h5 className='text-uppercase fw-bold mb-4'>Contact Us</h5>
+        <h5 className='text-uppercase fw-bold mb-4'>Connect with Us</h5>
         <form action=''>
     <MDBRow>
       
 
-      <MDBCol md='7' size='12' className='mb-4 mb-md-0'>
+      <MDBCol size='auto' className='mb-4 mb-md-0'>
         <MDBInput type='email' id='email1' label='' placeholder='Enter your email' />
       </MDBCol>
 
-      <MDBCol size='auto' className='mb-4 mb-md-0'>
+      <MDBCol size='auto' className='mb-4 mb-md-0 ms-auto'>
         <button className='btn btn-primary'>Subscribe</button>
       </MDBCol>
     </MDBRow>
@@ -106,13 +95,7 @@ function Footer() {
   
       <br />
         <a href='' className='ms-4 me-4 text-reset'>
-      <MDBIcon fab icon="facebook-f" />
-    </a>
-    <a href='' className='me-4 text-reset'>
-      <MDBIcon fab icon="twitter" />
-    </a>
-    <a href='' className='me-4 text-reset'>
-      <MDBIcon fab icon="google" />
+        <MDBIcon fab icon="google" />
     </a>
     <a href='' className='me-4 text-reset'>
       <MDBIcon fab icon="instagram" />
