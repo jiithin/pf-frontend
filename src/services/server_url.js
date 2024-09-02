91d51d0bfa75e00c126a02fc8f6281308348e015
Jithin <153214952+jiithin@users.noreply.github.com>
2024-09-02 17:52:04 +0530

    Update server_url.js

--- src/services/server_url.js
+++ src/services/server_url.js
@@ -1,2 +1,2 @@
-export const SERVER_URL="http://localhost:3000"
-//export const SERVER_URL="https://pf-backend-z8vq.onrender.com"
+//export const SERVER_URL="http://localhost:3000"
+export const SERVER_URL="https://pf-backend-z8vq.onrender.com"
